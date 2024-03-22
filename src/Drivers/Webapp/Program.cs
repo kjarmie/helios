0379f39a4e877fabe42f34ab9f60e3097ce0a527
kjarmie <kjarmie@gmail.com>
2024-03-22 13:32:50 +0200

    Website - Added favicon and static file hosting - Added serilog logging - Added root return endpoint handler

--- src/Drivers/Webapp/Program.cs
+++ src/Drivers/Webapp/Program.cs
@@ -1,7 +1,67 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Serilog;
 
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    var services = builder.Services;
+    var config = builder.Configuration;
 
-app.UseHttpsRedirection();
+    // Logging
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(config)
+        .CreateLogger();
 
-app.Run();
+    Log.Information("Building application");
+
+    builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+    // Routing
+    services.AddRouting(options => { options.LowercaseUrls = true; });
+
+    // App
+    var app = builder.Build();
+
+    // Logging
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+        };
+        options.IncludeQueryInRequestPath = true;
+    });
+
+    app.MapGet("/", () => Results.Content(
+        """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <link rel="icon" type="image/x-icon" href="favicon.svg">
+                <title>Helios</title>
+            </head>
+            <body>
+            Hello World!
+            </body>
+            </html>
+        """, "text/html")
+    );
+
+    app.UseStaticFiles();
+
+    app.UseHttpsRedirection();
+
+    // Run application
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
